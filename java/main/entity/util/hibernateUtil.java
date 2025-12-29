a0f6b20b6301d4403a08515778809932851cb91e
Tassokhan123 <ktasneem427@gmail.com>
2025-12-29 11:30:21 +0530

    Update hibernateUtil.java

---
+++ java/main/entity/util/hibernateUtil.java
@@ -0,0 +1,20 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                configuration.configure("hibernate.cfg.xml");
+                configuration.addAnnotatedClass(Task.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties())
+                        .build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Throwable ex) {
+                throw new Exception
